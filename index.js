42bf0f515c7b47a9f097973d72cd137eb1433365
joeljdv <joeldiazjdv@gmail.com>
2021-03-21 12:50:49 -0400

    commit tree 291026d22dcf9e9cf9fee78e15e207e53eafbb28

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(arr,name){
+    let list=[];
+    for(let i=0;i <arr.length;i++){
+        if(arr[i].toLowerCase()===name.toLowerCase()){
+            list.push(arr[i])
+        }
+    }
+    return list;
+}
+
+function fuzzyMatch(arra,name){
+    let fuzzy=[];
+    for(let i=0;i<arra.length;i++){
+        if(arra[i].charAt(0)===name.charAt(0)){
+            fuzzy.push(arra[i])
+        }
+    }
+    return fuzzy;
+}
+
+
+const driver = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(arr,name){
+    let match=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].name===name){
+            match.push(arr[i])
+        }
+    }
+    return match;
+  }
